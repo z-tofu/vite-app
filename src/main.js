5a0c6c8712a8fe09603734390888cd859afc8da8
Christoffer Gräsbeck <christoffer.grasbeck@gmail.com>
2025-09-18 09:43:54 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,105 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+
+const geometry = new THREE.TorusGeometry(10,3, 16, 100);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xFF6347,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(10, 10, 10);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff, emissive: 0xffffff, emissiveIntensity: 3})
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+
+}
+
+Array(200).fill().forEach(addStar);
+
+
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+spaceTexture.colorSpace = THREE.SRGBColorSpace;
+scene.background = spaceTexture;
+
+
+function moveCamera(){
+
+  const t = document.body.getBoundingClientRect().top;
+  
+  camera.position.z = t* -0.01;
+  camera.position.y = t* -0.0002;
+  camera.position.x = t* -0.0002;
+}
+document.body.onscroll = moveCamera;
+
+
+let lockFramerate = false;
+window.addEventListener("keydown", (e) => {
+  if (e.key === "f") {
+    lockFramerate = !lockFramerate;
+    console.log("Lock framerate:", lockFramerate);
+  }
+});
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  const now = performance.now();
+  const delta = now - (animate.lastTime || 0);
+
+  if (!lockFramerate || delta > 1000/60) {
+
+    
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    if (lockFramerate){
+      animate.lastTime = now;
+    }
+}
+
+}
+
+animate()
